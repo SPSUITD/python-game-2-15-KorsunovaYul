cec985dc405c50be720dcb3a9c52039f1a424d10
Yulia Korsunova <124903683+KorsunovaYul@users.noreply.github.com>
2024-03-23 02:03:32 +0300

    Add files via upload

---
+++ 1/fon1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fon1" tilewidth="320" tileheight="320" tilecount="28" columns="7">
+ <image source="../fon1.png" width="2240" height="1280"/>
+</tileset>
---
+++ game.py
@@ -0,0 +1,122 @@
+"""
+Platformer Game
+"""
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Platformer"
+
+CHARACTER_SCALING = 0.5
+TILE_SCALING = 0.5
+
+
+PLAYER_MOVEMENT_SPEED = 5
+GRAVITY = 0
+
+class MyGame(arcade.Window):
+
+    def __init__(self):
+
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+        self.scene = None
+        self.player_sprite = None
+        self.physics_engine = None
+        self.camera = None
+        self.gui_camera = None
+        self.tile_map = None
+
+        arcade.set_background_color(arcade.csscolor.CORNFLOWER_BLUE)
+
+    def setup(self):
+
+        self.camera = arcade.Camera()
+        self.gui_camera = arcade.Camera()
+
+        map_name = "1/fon..json"
+
+        layer_options = {
+            "table": {
+                "use_spatial_hash": True,
+            },
+        }
+
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        image_source = "1/Glavniy.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 300
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
+
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite,gravity_constant=GRAVITY, walls=self.scene["table"]
+        )
+
+    def on_draw(self):
+
+        self.clear()
+
+        self.camera.use()
+
+        # Draw our Scene
+        self.scene.draw()
+
+        self.gui_camera.use()
+
+    def on_key_press(self, key, modifiers):
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+
+        if key == arcade.key.UP or key == arcade.key.W:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
+
+        if screen_center_x < 0:
+            screen_center_x = 0
+
+        if screen_center_y < 0:
+            screen_center_y = 0
+
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
+    def on_update(self, delta_time):
+
+        self.physics_engine.update()
+        self.center_camera_to_player()
+
+
+def main():
+    window = MyGame()
+    window.setup()
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
