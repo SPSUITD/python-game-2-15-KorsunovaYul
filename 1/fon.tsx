8c66df6d9f6eb829c004117abe055aaaed6b1a24
Yulia <korsyulia@yandex.ru>
2024-04-20 22:56:58 +0300

    настроила hitbox
    доделала фон с границами, чтобы не уйти за рамки

---
+++ 1/fon.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="fon" tilewidth="160" tileheight="160" tilecount="77" columns="7">
+ <image source="fon.png" width="1120" height="1760"/>
+</tileset>
